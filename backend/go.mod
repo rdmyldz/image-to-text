222c4fe8a8c920791afd7015aba8cd179ecf05f2
rdmyldz <n.erdemyildiz@gmail.com>
2022-05-09 01:19:31 +0300

    backend: Add cancellation support

--- backend/go.mod
+++ backend/go.mod
@@ -2,7 +2,9 @@ module github.com/image-to-text/backend
 
 go 1.17
 
+replace github.com/rdmyldz/i2t => /home/rdmyldz/repos/github.com/rdmyldz/i2t
+
 require (
 	github.com/gorilla/mux v1.8.0
-	github.com/rdmyldz/i2t v0.0.0-20220421211800-e20d399ef024
+	github.com/rdmyldz/i2t v0.0.0-20220508215353-2c205fbe57d0
 )
--- backend/main.go
+++ backend/main.go
@@ -33,24 +33,28 @@ func (a *application) handleHome(w http.ResponseWriter, r *http.Request) {
 	log.Printf("error: %v", err)
 }
 
+type result struct {
+	text []byte
+	err  error
+}
+
 func (a *application) handleUpload(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
 	start := time.Now()
 	defer func(t time.Time) {
 		log.Printf("since: client: %s - %v\n", time.Since(t), r.RemoteAddr)
 	}(start)
 
-	handle, err := tesseract.TessBaseAPICreate("tur+eng")
+	handle, err := tesseract.TessBaseAPICreateWithMonitor("tur+eng")
 	if err != nil {
 		log.Println(err)
 		return
 	}
+
 	w.Header().Set("Access-Control-Allow-Origin", "*")
-	// w.Header().Set("Access-Control-Allow-Methods", "POST, GET ")
-	// w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization ")
 
 	r.ParseMultipartForm(10 << 20)
 
-	// Get handler for filename, size and headers
 	file, handler, err := r.FormFile("file")
 	if err != nil {
 		fmt.Println("Error Retrieving the File")
@@ -69,21 +73,43 @@ func (a *application) handleUpload(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	fmt.Printf("fsize in handler: %v\n", len(f))
-	texts, err := handle.ProcessImageMem(f)
-	if err != nil {
-		log.Println("error ProcessImageMem: ", err)
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
 
-	log.Printf("path: %q\n", r.URL.Path)
-	d := data{Content: texts}
-	jsonResp, err := json.Marshal(d)
-	if err != nil {
-		log.Fatalf("Error happened in JSON marshal. Err: %s", err)
+	ch := make(chan result)
+	go func() {
+		defer close(ch)
+		fmt.Printf("fsize in handler: %v\n", len(f))
+		texts, err := handle.ProcessImageMem(f)
+		if err != nil {
+			ch <- result{text: nil, err: err}
+			return
+		}
+
+		log.Printf("path: %q\n", r.URL.Path)
+		d := data{Content: texts}
+		jsonResp, err := json.Marshal(d)
+		if err != nil {
+			ch <- result{text: nil, err: err}
+			return
+		}
+
+		ch <- result{text: jsonResp, err: err}
+	}()
+
+	select {
+	case <-ctx.Done():
+		log.Println("context done")
+		handle.SetCancelFunc()
+		<-ch // on cancellation, wait for ProcessImageMem to finish
+		handle.End()
+		handle.Delete()
+	case r := <-ch:
+		if r.err != nil {
+			log.Println("error ProcessImageMem: ", err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Write(r.text)
 	}
-	w.Write(jsonResp)
 }
 
 func (app *application) routes() http.Handler {
@@ -102,11 +128,6 @@ func (app *application) routes() http.Handler {
 // for now, carried to create the tesseract instance into handleUpload
 // since this way, we can't convert images to texts concurrently
 func main() {
-	// handle, err := tesseract.TessBaseAPICreate("tur+eng")
-	// if err != nil {
-	// 	log.Fatalln(err)
-	// }
-
 	addr := flag.String("addr", ":8080", "http network address")
 	app := &application{
 		// handle: handle,
@@ -116,8 +137,7 @@ func main() {
 		Addr:    *addr,
 		Handler: app.routes(),
 	}
-	// router :=mux.NewRouter()
-	// router.HandleFunc("/", app.handleHome).Methods("GET")
+
 	log.Printf("starting server on %s\n", srv.Addr)
 	log.Fatalln(srv.ListenAndServe())
 }
